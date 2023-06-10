7ee4a99a2384a82d9357c3759273218711078d51
Pavel Gorbunov <darnelo@inbox.ru>
2023-06-10 21:13:36 +0300

    changed types

--- ui/src/API/chessApi.ts
+++ ui/src/API/chessApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { ISession } from "../models/Session"
+import { SessionDto } from "../generated/api"
 
 export const chessApi = createApi({
   reducerPath: "chessApi",
@@ -7,7 +7,7 @@ export const chessApi = createApi({
     baseUrl: "http://w1vvv1w.fvds.ru:81/",
   }),
   endpoints: (build) => ({
-    getSession: build.query<ISession, string>({
+    getSession: build.query<SessionDto, string>({
       query: (id: string) => `session/db5f6aea-22da-4cd3-93b4-885c987a22d0`,
       // `session/${id}`,
     }),
