e93e7a978627bf2247bd95aad350a6ee6a1a0c47
Pavel Gorbunov <darnelo@inbox.ru>
2023-07-08 20:32:05 +0500

    auth without semding a req

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
+
         <Route path="/auth" element={<Auth />} />
       </Routes>
     </>
--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1 className="center white">Chess</h1>
+  return <h1 className="center white page">Chess</h1>
 }
 
 export default Home
