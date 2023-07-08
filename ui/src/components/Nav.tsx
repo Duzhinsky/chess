18b79f08e024075563b1e50552896f0f9253124c
Pavel Gorbunov <darnelo@inbox.ru>
2023-07-08 18:58:16 +0500

    axios removed (again)

--- ui/src/components/Nav.tsx
+++ ui/src/components/Nav.tsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom"
 const Nav: FC = () => {
   return (
     <div className="nav">
+      <Link className="nav__item" to="/auth">
+        Login
+      </Link>
       <Link className="nav__item" to="/">
         Home
       </Link>
