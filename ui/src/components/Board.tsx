127d5f892375396b074567c673a3b7d2e8b4d0df
Pavel Gorbunov <darnelo@inbox.ru>
2023-07-10 19:48:00 +0500

    little patch that solves big problem

--- ui/src/components/Board.tsx
+++ ui/src/components/Board.tsx
@@ -1,16 +1,17 @@
 import { FC, Fragment, useLayoutEffect } from "react"
 import { useLazyGetSessionQuery } from "../API/chessApi"
-import { useAppSelector } from "../hooks/reduxHooks"
+import { useActions, useAppSelector } from "../hooks/reduxHooks"
 import Cell from "./Cell"
 import { useClickHandler } from "../hooks/useClickHandler"
 import { id } from "../utils/id"
 
 const Board: FC = () => {
   const cells = useAppSelector((state) => state.cells)
+  const { setSelectedCell, highlightMoves } = useActions()
 
   const [getSession] = useLazyGetSessionQuery()
 
-  const clickHandler = useClickHandler()
+  const clickHandler = useClickHandler(setSelectedCell, highlightMoves)
 
   useLayoutEffect(() => {
     getSession(id)
@@ -24,7 +25,7 @@ const Board: FC = () => {
             <Cell
               key={JSON.stringify(cell.position)}
               {...cell}
-              clickHandler={clickHandler}
+              clickHandler={() => clickHandler(cell.position)}
             />
           ))}
         </Fragment>
--- ui/src/hooks/useClickHandler.ts
+++ ui/src/hooks/useClickHandler.ts
@@ -1,15 +1,28 @@
+import { useMakeMoveMutation } from "../API/chessApi"
 import { PositionDto } from "../generated/api"
-import { useActions } from "./reduxHooks"
-import { useMove } from "./useMove"
+import { id } from "../utils/id"
+import { useAppSelector } from "./reduxHooks"
 
-export const useClickHandler = () => {
-  const { setSelectedCell, highlightMoves } = useActions()
+export const useClickHandler = (
+  setSelectedCell: (position: PositionDto | null) => void,
+  highlightMoves: (position: PositionDto) => void
+) => {
+  const [makeMove] = useMakeMoveMutation()
+  const cells = useAppSelector((state) => state.cells)
 
   return function useInside(position: PositionDto) {
     setSelectedCell(position)
-
     highlightMoves({ x: position.x, y: position.y })
 
-    useMove(position)
+    if (cells.cells[position.y][position.x].available) {
+      const indexId = cells.moves.findIndex(
+        (move) => move.to.x === position.x && move.to.y === position.y
+      )
+      makeMove({
+        id,
+        moveId: cells.moves[indexId].id,
+      })
+      setSelectedCell(null)
+    }
   }
 }
