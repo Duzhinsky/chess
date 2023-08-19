75ead64bdd8f1e4773dbd95637bc94fa41dee781
Pavel Gorbunov <darnelo@inbox.ru>
2023-08-19 11:44:31 +0500

    auth page removed, auth modal added

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Game from "./pages/Game"
 import Home from "./pages/Home"
-import Auth from "./pages/Auth"
 import Nav from "./components/Nav"
 import "./App.css"
 
@@ -12,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
-
-        <Route path="/auth" element={<Auth />} />
       </Routes>
     </>
   )
