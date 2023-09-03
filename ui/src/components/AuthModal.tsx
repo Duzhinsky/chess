268d7f194db03d519673729448c8364603e443a1
Pavel Gorbunov <darnelo@inbox.ru>
2023-09-03 05:45:02 +0300

    oauth2 token - start

--- ui/src/components/AuthModal.tsx
+++ ui/src/components/AuthModal.tsx
@@ -1,12 +1,29 @@
-import React from "react"
+import { FC } from "react"
 import { Modal, Form, Button } from "antd"
+import { useOAuth2 } from "@tasoskakour/react-use-oauth2"
 import vk_icon from "../imgs/icons/vk.svg"
 import { AuthModalProps } from "../models/Modal"
 
-const AuthModal: React.FC<AuthModalProps> = ({ open, onCancel }) => {
+const AuthModal: FC<AuthModalProps> = ({ open, onCancel }) => {
+  const { data, loading, error, getAuth, logout } = useOAuth2({
+    authorizeUrl: "https://oauth.vk.com/authorize",
+    clientId: "51731397",
+    redirectUri: `http://chess.duzhinsky.ru/login`,
+    scope: "email",
+    responseType: "code",
+    exchangeCodeForTokenServerURL: "https://your-backend/token",
+    exchangeCodeForTokenMethod: "POST",
+    onSuccess: (payload) => console.log("Success", payload),
+    onError: (error_) => console.log("Error", error_),
+  })
+
+  const isLoggedIn = Boolean(data?.access_token)
+
   //const { toggleAuthModal } = useActions()
   const handleVKAuth = () => {
-    // Обработка авторизации через VK
+    console.log("Submit")
+    getAuth()
+    // await Обработка авторизации через VK
     // Если всё ок toggleAuthModal()
   }
 
