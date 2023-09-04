91dd93161857cbcf4db5fee361d6b44d94b9b26c
Dmitrii Duzhinskii <w1vvv1w@ya.ru>
2023-09-04 18:11:55 +0300

    auth

--- api/main/build.gradle.kts
+++ api/main/build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     // Spring Web
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.1.3")
+    implementation("org.springframework.security:spring-security-web:6.1.3")
+    implementation("org.springframework.security:spring-security-oauth2-client:6.1.3")
+    implementation("org.springframework.security:spring-security-oauth2-jose:6.1.3")
+
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
---
+++ api/main/src/main/java/duzhinsky/chess/api/SecurityConfig.java
@@ -0,0 +1,46 @@
+package duzhinsky.chess.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain configureSecurity(HttpSecurity security, CustomOAuth2UserService customOAuth2UserService) throws Exception {
+        security
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login(oa2 ->
+                        oa2.tokenEndpoint().accessTokenResponseClient(accessTokenResponseClient())
+                                .and()
+                                .userInfoEndpoint().userService(customOAuth2UserService));
+        return security.build();
+    }
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient =
+                new DefaultAuthorizationCodeTokenResponseClient();
+        OAuth2AccessTokenResponseHttpMessageConverter tokenResponseHttpMessageConverter =
+                new OAuth2AccessTokenResponseHttpMessageConverter();
+        tokenResponseHttpMessageConverter.setAccessTokenResponseConverter(new CustomTokenResponseConverter());
+        RestTemplate restTemplate = new RestTemplate(Arrays.asList(
+                new FormHttpMessageConverter(), tokenResponseHttpMessageConverter));
+        restTemplate.setErrorHandler(new OAuth2ErrorResponseErrorHandler());
+        accessTokenResponseClient.setRestOperations(restTemplate);
+        return accessTokenResponseClient;
+    }
+}
